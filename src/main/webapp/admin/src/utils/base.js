1f9ec3d89b24ffce9aad1e73cba18ccd12913c63
small-bears <3112420979@qq.com>
2023-02-12 17:42:48 +0800

    基于SSM框架的宠物领养系统

---
+++ src/main/webapp/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/ssmqj7b0/",
+            name: "ssmqj7b0",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssmqj7b0/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "宠物管理系统"
+        } 
+    }
+}
+export default base
